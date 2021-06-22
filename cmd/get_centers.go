68eef2705ee10550aeec1bf37a0a12c381fc7f17
Boopathy Sivakumar <>
2021-06-22 11:02:18 +0530

    Adding functionallity to search the centers available

--- cmd/get_centers.go
+++ cmd/get_centers.go
@@ -1,4 +1,4 @@
-// /*
+//
 // Copyright © 2021 NAME HERE <EMAIL ADDRESS>
 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// */
+//
 //
 package cmd
 
@@ -172,19 +172,3 @@ func getCentersCmd() *cobra.Command {
 
 	return cmd
 }
-
-// func init() {
-// 	getCmd.AddCommand(getCentersCmd)
-
-// 	// Here you will define your flags and configuration settings.
-
-// 	// Cobra supports Persistent Flags which will work for this command
-// 	// and all subcommands, e.g.:
-// 	// getCentersCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-// 	// Cobra supports local flags which will only run when this command
-// 	// is called directly, e.g.:
-// 	// getCentersCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-// 	getCentersCmd.Flags().StringVarP(&filterBy, "source", "s", "", "Source directory to read from")
-
-// }
